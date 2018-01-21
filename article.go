33d5eab085879f74bc37329c00eac19646982634
Stanney <freehourse2sh@gmail.com>
2018-01-21 23:18:44 +0800

    return json format articles

--- article.go
+++ article.go
@@ -3,10 +3,10 @@ package main
 import "time"
 
 type Article struct {
-	Title       string
-	Description string
-  Status      bool
-	CreatedAt         time.Time 
+	Title       string `json:"title"`
+	Description string `json:"description"`
+  Status      bool   `json:"status"`
+	CreatedAt   time.Time `json:"createdAt"`
 }
 
 type Articles []Article
